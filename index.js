e27dcb1b6aef5a71e5939adf4f2ee2217f979ecb
Hanna Strezh <hannastrezh@gmail.com>
2018-07-24 16:48:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function appendKitten(name){
 function prependKitten(name){
   return [name].concat(kittens);// [name] is the way to put the array in front //of the concat
 }
-function removeLastKitten(){
+function removeLastKitten(name){
   return kittens.slice(-1);
 }
